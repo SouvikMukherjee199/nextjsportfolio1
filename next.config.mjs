56305832240384ffaf8048445523d81342bb66e7
souvikmukherjee199 <svkmukherjee1@gmail.com>
2024-08-13 20:25:41 +0530

    Rectified

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript:{
-        output:"export",
+        // output:"export",
         ignoreBuildErrors : true
     }
 };
